2e1ec46e59213213144995ebfc090b416b6d59fd
RodTeixeira <rodofikis@hotmail.com>
2023-08-26 21:10:55 -0300

    create projetc

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Rodolfo on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
